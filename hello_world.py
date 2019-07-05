4ef671b8a1544fbe77b426930d6620e11bbccd2d
WarGamezz <widofmaker@gmail.com>
2019-07-05 10:46:47 -0400

    Additional non-important changes

--- hello_world.py
+++ hello_world.py
@@ -13,18 +13,6 @@
 
 #Whether output is buffered is usually determined by file, but if the flush keyword argument is true, the stream is forcibly flushed.
 
-
-print('Hello world. This works') #single quoatation works, you can use it if you want to use double quotation in the string without escaping anything
-print("Hello world. This also works.") #double quoatation works, you can use it if you want to use single quotation in the string without escaping anything
-print("""Printing over multiple lines 
-requires triple quotation
-and it works and works and works""") #triple quotation (whether single or double) allows you to wrinte strings over multiple lines
-string1 = "Hello "
-string2 = "world"
-print(string1 + string2) #you can simply add strings together by using + signs in between
-print(len(string1)) # you can figure out the length of the string using len(string)
-print(string1[0:3]) # you can access individual index or range of indexes. First index is inclusive, second one is not. 
-
 #? print syntax
 # print(object(s), separator=separator, end=end, file=file, flush=flush)
 # "separator" - Specify how to separate the objects, if there is more than one. Default is ' '
@@ -35,10 +23,41 @@ print("Hello", "how are you?", end = " **************** ")
 print('Hello', 'World', 2+3, file=open('file.txt', 'w'))
 
 
+print('Hello world. This works') #single quoatation works, you can use it if you want to use double quotation in the string without escaping anything
+print("Hello world. This also works.") #double quoatation works, you can use it if you want to use single quotation in the string without escaping anything
+print("""Printing over multiple lines 
+requires triple quotation
+and it works and works and works""") #triple quotation (whether single or double) allows you to wrinte strings over multiple lines
+string1 = "Hello "
+string2 = "world"
+print(string1 + string2) #you can simply add strings together by using + signs in between
+print(len(string1)) # you can figure out the length of the string using len(string)
 
-************
-Testing something out over here
+#! Slice
+string1 = "Hello World"
+#Return a slice object representing the set of indices specified by range(start, stop, step)
+#The slicing operator [] is actually being used in the above code with a slice() object using the : notation (which is only valid within []), i.e.:
+#a[start:stop:step] is equivalent to a[slice(start, stop, step)]
+print(string1[0:3]) # you can access individual index or range of indexes. First index is inclusive, second one is not. 
+print(string1[2]) #here we acces 3 character (3rd index)
+print(string1[::2]) #print from the first index to the last, every other character
+#step might be a negative number for example
+print(string1[::-1])    # all items in the array, reversed
+print(string1[1::-1])   # the first two items, reversed
+print(string1[:-3:-1])  # the last two items, reversed
+print(string1[-3::-1])  # everything except the last two items, reversed
 
-string_test = "Hello world"
-print(string_test)
-print(string_test[::-1])
+#! String methods
+string1 = "thIs Is the soNg that neVer enDs"
+#str.capitalize Return a copy of the string with its first character capitalized and the rest lowercased.
+print(string1.capitalize())
+#str.count Return the number of non-overlapping occurrences of substring sub in the range [start, end]. Optional arguments start and end are interpreted as in slice notation.
+print(string1.count("e")) # letter e shows up for time
+#endswith Return True if the string ends with the specified suffix, otherwise return False. (don't have good example yet)
+#str.expandtabs Return a copy of the string where all tab characters are replaced by one or more spaces, depending on the current column and the given tab size.
+print('01\t012\t0123\t01234'.expandtabs())
+print('01\t012\t0123\t01234')
+#str.find Return the lowest index in the string where substring sub is found within the slice s[start:end]. Optional arguments start and end are interpreted as in slice notation. Return -1 if sub is not found.
+print('Tell me where is the first l in this string'.find('l')) #using index
+#The find() method should be used only if you need to know the position of sub. To check if sub is a substring or not, use the in operator:
+print('first' in 'Tell me where is the first l in this string')
